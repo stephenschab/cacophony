665ec0c7d156b52ab860162f3d4e00434e27805a
stephenschab <sschab689@gmail.com>
2019-12-30 08:49:57 -0700

    initial commit

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import NewUserForm from '../NewUserForm/NewUserForm';
+
+function App() {
+  return (
+    <div className="App">
+
+      <nav className="navbar">
+        <p className="app-logo">Cacophony</p>
+      </nav>
+
+      <NewUserForm />
+      
+    </div>
+  );
+}
+
+export default App;
