c4de118d309d8452e1967d2a4192a7628542a135
stephenschab <sschab689@gmail.com>
2020-01-08 09:19:43 -0700

    added PrivateRoute component and addedall routes to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import PublicOnlyRoute from '../Utils/PublicOnlyRoute';
@@ -6,12 +6,28 @@ import LandingPage from '../../routes/LandingPage/LandingPage';
 import RegisterPage from '../../routes/RegisterPage/RegisterPage';
 import LoginPage from '../../routes/LoginPage/LoginPage';
 import Navbar from '../Navbar/Navbar';
+import PrivateRoute from '../Utils/PrivateRoute';
 
-class App extends React.Component {
+export default class App extends Component {
+  state = {
+    hasError: false
+  }
+
+  static getDerivedStateFromError(error) {
+    console.error(error)
+    return { hasError: true}
+  }
+
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <header classname='App__header'>
+          <Navbar />
+        </header>
+
+        <main className='App__main'>
+          {this.state.hasError && <p className='error'></p>}
+        </main>
   
         <Switch>
           <Route
@@ -23,17 +39,23 @@ class App extends React.Component {
             path='/new-user'
             component={RegisterPage}
           />
-          <Route
+          <PublicOnlyRoute
             path='/login'
             component={LoginPage}
           />
+          <PrivateRoute
+            path='/posts'
+            component={PostsListPage}
+          />
+          <PrivateRoute
+            path='/post/:post'
+            component={PostPage}
+          <Route
+            component={NotFoundPage}
+          />
         </Switch>
-        
-  
       </div>
     );
   }
   
 }
-
-export default App;
